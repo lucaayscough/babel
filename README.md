74c71173083ec98274c71e0d9cfe0b48739e4807
lucaayscough <lucaayscough@gmail.com>
2024-04-22 21:18:05 +0100

    rendered tiled font bitmap

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple text editor
+
+## Dependencies
+
+- SDL: https://github.com/libsdl-org/SDL
+
+## Assets
+
+- Font: http://www.bay12forums.com/smf/index.php?topic=158914.0
+
--- src/main.c
+++ src/main.c
@@ -10,24 +10,40 @@ void scc(int code)
   }
 }
 
-void scp(void* ptr)
+void* scp(void* ptr)
 {
   if (!ptr)
   {
     fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
     exit(1);
   }
+
+  return ptr;
 }
 
 int main(void)
 {
   scc(SDL_Init(SDL_INIT_VIDEO));
 
-  SDL_Window* window = SDL_CreateWindow("Babel",
-                                        SDL_WINDOWPOS_CENTERED,
-                                        SDL_WINDOWPOS_CENTERED,
-                                        1280, 720, 0);
-  scp(window);
+  SDL_Window* window = scp(SDL_CreateWindow("Babel",
+                                            SDL_WINDOWPOS_CENTERED,
+                                            SDL_WINDOWPOS_CENTERED,
+                                            1920, 1080, 0));
+  
+  SDL_Renderer* renderer = scp(SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE));
+  SDL_Surface* surface = scp(SDL_LoadBMP("../assets/font.bmp"));
+  SDL_Texture* texture = scp(SDL_CreateTextureFromSurface(renderer, surface));
+
+  SDL_Rect rect;
+  rect.x = 0;
+  rect.y = 0;
+  rect.w = 1024;
+  rect.h = 1024;
+
+  scc(SDL_RenderCopy(renderer, texture, NULL, &rect));
+  SDL_RenderPresent(renderer);
+
+
 
   bool running = true;
   SDL_Event event;
@@ -38,6 +54,9 @@ int main(void)
     {
       switch (event.type)
       {
+        case SDL_QUIT:
+          running = false;
+          break;
         case SDL_KEYDOWN:
           running = false;
           break;
@@ -45,10 +64,6 @@ int main(void)
           break;
       };
     }
-    
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    scp(surface);
-    scc(SDL_UpdateWindowSurface(window));
   }
 
   SDL_Quit();
